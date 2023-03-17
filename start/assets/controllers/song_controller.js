9a0856582c9f8eaf29bdf03a4ab111cecc3b79fa
TrosseroAgustin01 <trosseroagustin01@gmail.com>
2023-03-17 11:27:33 -0300

    another act

--- start/assets/controllers/song_controller.js
+++ start/assets/controllers/song_controller.js
@@ -1,16 +1,23 @@
 import { Controller } from '@hotwired/stimulus';
-
+import axios from "axios";
 /*
  * This is an example Stimulus controller!
  *
  * Any element with a data-controller="hello" attribute will cause
  * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
+ * song_controller.js -> "hello"
  *
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+
+    static values = {
+        infoUrl: String
+    }
+    play(event) {
+        event.preventDefault();
+
+        console.log(this.infoUrlValue);
+        //axios.get(infoUrl);
     }
 }
